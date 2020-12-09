77efdc2193305b4896d9e1bd309cc84bee7331b6
Fabrice Castel <fabrice.castel@hotmail.com>
2020-12-08 19:59:34 -0800

    Minor aesthetic tweaks

--- keymap.c
+++ keymap.c
@@ -17,10 +17,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |   ,  |   .  |   /  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      | GUI  |Shift |Bsp/L1|             |Spc/L2|'/Ctl | Tab  |      |      |
+   * |      |      | GUI  |Shift |Bsp/L1|      |      |Spc/L2|'/Ctl | Tab  |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-	[_L0] = LAYOUT_ortho_4x12(
+  [_L0] = LAYOUT_ortho_4x12(
     KC_Q,  KC_W,  KC_E,    KC_R,    KC_T,   KC_NO, KC_NO, KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,
     KC_A,  KC_S,  KC_D,    KC_F,    KC_G,   KC_NO, KC_NO, KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN,
     KC_Z,  KC_X,  KC_C,    KC_V,    KC_B,   KC_NO, KC_NO, KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |   !  |   @  |   #  |   $  |   %  |      |      |   ^  |   &  |   *  |   +  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |######|             |Enter |Shift |      |      |      |
+   * |      |      |      |      |######|      |      |Enter |Shift |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-	[_L1] = LAYOUT_ortho_4x12(
+  [_L1] = LAYOUT_ortho_4x12(
     KC_1,    KC_2,  KC_3,    KC_4,    KC_5,    KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_LBRC, KC_LT, KC_LCBR, KC_LPRN, KC_MINS, KC_NO, KC_NO, KC_EQL,  KC_RPRN, KC_RCBR, KC_GT,   KC_RBRC,
     KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_NO, KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_NO,
@@ -51,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |   /  |   *  |   -  |   +  |   =  |      |      | Play | Vol- | Vol+ | Bri- | Bri+ |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |Shift | Ctl  |             |######|      |      |      |      |
+   * |      |      |      |Shift | Ctl  |      |      |######|      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-	[_L2] = LAYOUT_ortho_4x12(
+  [_L2] = LAYOUT_ortho_4x12(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO, KC_NO, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_GRV,  KC_BSLS, KC_PIPE, KC_SLSH, KC_UNDS, KC_NO, KC_NO, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     KC_SLSH, KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_NO, KC_NO, KC_MPLY, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU,
