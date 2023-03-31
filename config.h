a39cf55ad1c5d75111de3a363899d5d0ceff26de
Fabrice Castel <fabrice.castel@hotmail.com>
2023-03-30 21:56:39 -0700

    Add mod key taps to the home row, window navigation shortcuts and more convenient tab key placement

--- config.h
+++ config.h
@@ -1,2 +1,6 @@
+#define BILATERAL_COMBINATIONS
 #define PERMISSIVE_HOLD
-#define TAPPING_TERM 160
+#define TAPPING_TERM 180
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
